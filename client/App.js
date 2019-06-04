ca78bb6cc74c95047655af850c6d8dfb8b9bd19f
Subho Karmakar <isubhokarma@gmail.com>
2019-06-04 07:03:35 +0530

    App/ Header with Styles

---
+++ client/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import Header from "./src/components/Header";
+
+const App = () => {
+  return <Header headerText={"Home"} />;
+};
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as client } from "./app.json";
+
+AppRegistry.registerComponent(client, () => App);
